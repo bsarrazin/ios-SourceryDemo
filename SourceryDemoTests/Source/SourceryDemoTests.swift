e91150254b089c77b3ae1feec0775b60708f1156
Ben Sarrazin <ben@7swift.io>
2017-10-25 17:45:19 -0400

    Adding failing tests

--- SourceryDemoTests/Source/SourceryDemoTests.swift
+++ SourceryDemoTests/Source/SourceryDemoTests.swift
@@ -3,7 +3,25 @@ import XCTest
 
 class SourceryDemoTests: XCTestCase {
     
+    func test_PetEquality() {
+        let pet = Pet(kind: .dog, name: "Rex", age: 2)
+        let sut = Pet(kind: .dog, name: "Rex", age: 2)
+        XCTAssertTrue(sut == pet)
+    }
+    
     func test_PersonEquality() {
+        let pet = Pet(kind: .dog, name: "Rex", age: 2)
+        let person = Person(firstName: "Clark",
+                            lastName: "Kent",
+                            age: 32,
+                            pets: [pet])
+        
+        let sut = Person(firstName: "Clark",
+                            lastName: "Kent",
+                            age: 32,
+                            pets: [Pet(kind: .dog, name: "Rex", age: 2)])
+        
+        XCTAssertTrue(sut == person)
     }
     
 }
